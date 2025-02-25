454ecb018cb892da0707e7b1163087a13172941b
Nicolas <bircni@icloud.com>
2025-02-25 18:09:27 +0100

    Update Rust edition (#2)
    * update Cargo.toml
    
    * bump to edition2024
    
    * add tests

--- src/icon.rs
+++ src/icon.rs
@@ -16,3 +16,26 @@ impl From<Icon> for Cow<'static, str> {
         Cow::from(icon.url())
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const FA_VERSION: &str = "6.7.2";
+    #[test]
+    fn test_display() {
+        assert_eq!(
+            Icon::RUST.to_string(),
+            "https://site-assets.fontawesome.com/releases/v6.7.2/svgs/brands/rust.svg"
+        );
+    }
+
+    #[test]
+    fn test_into_cow() {
+        let icon: Cow<'static, str> = Icon::RUST.into();
+        assert_eq!(
+            icon,
+            "https://site-assets.fontawesome.com/releases/v6.7.2/svgs/brands/rust.svg"
+        );
+    }
+}
